6acebea1a4ba2fdf6820771bdffb4e11733d7b02
Kristofers5 <kristofers1104@inbox.lv>
2024-12-13 15:45:24 +0000

    possiblyWorkingLZ77comp

--- Test.java
+++ Test.java
@@ -1,80 +1,146 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Test {
 
-    public static void tokenMakingTest(String s1, String s2) {
-        String SearchBufferTest = s1;   // String SearchBufferTest = "sir sid eastman easily t";
-        String LookAheadTest = s2;      // String LookAheadTest = "eases sea sick seals    ";
+    public static void tokenMakingTest(String fileName) {
 
-        int lenghtOfBuffer = SearchBufferTest.length();
+        /*
+        String test = "sir sid eastman easily t";
+        byte test1[] = new byte[60];
 
-        int tokenOffset = 0;
-        int tokenLength = 0;
-        char tokenChar = ' ';
+        test1 = test.getBytes();
 
-        String tokenString = "";
+        for(byte b : test1){
+            System.out.print(b + " ");
+        }
+        */
 
-        for (int i = 0; i < lenghtOfBuffer; i++) { // iterates through the entire buffer
+        //byte test2[] = {115, 105, 114, 32, 115, 105, 100, 32, 101, 97, 115, 116, 109, 97, 110, 32, 101, 97, 115, 105, 108, 121, 32, 116};
 
-            String searchBufferChar = SearchBufferTest.substring(i, i + 1);
-            String lookAheadChar = LookAheadTest.substring(0, 1);
+        
+        try {
+            FileInputStream fin = new FileInputStream(fileName);
+            System.out.println("File opened successfully!");
 
-            if (searchBufferChar.equals(lookAheadChar)) {
+            byte searchBuffer[] = new byte[30000];
+            byte lookAheadBuffer[] = new byte[500];
 
-                System.out.println("position they are equal at = " + i);
+            int lenghtOfSearchBuffer = searchBuffer.length;
 
-                int k = 0;
-                int possibleTokenLength = 0;
-                while (SearchBufferTest.substring(i + k, i + k + 1).equals(LookAheadTest.substring(k, k + 1))) { // iterates through the following characters as long as they match in both buffers
 
-                    k++;
-                    possibleTokenLength++;
+            for(int i = 0; i < lookAheadBuffer.length-1; i++){          //fills look ahead buffer
+                lookAheadBuffer[i] = (byte)fin.read();
+            }
+
+
+
+            int tokenCount = 0;
+
+
+
+                while(lookAheadBuffer[0] != -1){                                                                                                                                //while(fin.read() != -1){
+
+
+                short tokenOffset = 0;
+                short tokenLength = 0;
+                byte tokenByte = ' ';
+
+
+                for(int i = 0; i < lenghtOfSearchBuffer-1; i++){
+                
+                    byte searchBufferByte = searchBuffer[i];
+                    byte lookAheadBufferByte = lookAheadBuffer[0];
+    
+    
+                    if(searchBufferByte == lookAheadBufferByte){
+                        
+
+                        //System.out.println("duplicate found");
+
+
+                        int k = 0;
+                        int potentionalTokenLength = 0;
+    
+                        while(searchBuffer[i+k] == lookAheadBuffer[k] && i+k < searchBuffer.length - 1){     //&& k < lookAheadBuffer.length - 1                                                                                                            //while(searchBuffer[i+k] == lookAheadBuffer[k] && i+k < 499){
+                            k++;
+                            potentionalTokenLength++;
+                            //System.out.println("duplicate found");
+                        }
+    
+                        if(potentionalTokenLength > tokenLength){
+                            tokenLength = (short)potentionalTokenLength;
+                            tokenOffset = (short)(lenghtOfSearchBuffer - i);
+                            tokenByte = lookAheadBuffer[k];
+
+                            //System.out.println("Test token = ( " + tokenOffset + ", " + tokenLength + ", " + tokenByte + " )");
+                        }
+    
+    
+                    }else if (tokenLength == 0){
+
+                        tokenLength = 0;
+                        tokenOffset = 0;
+                        tokenByte = lookAheadBuffer[0];                                         //tokenByte = lookAheadBuffer[1];
+                    }
+    
+    
+    
+    
+    
+    
                 }
-                if (possibleTokenLength > tokenLength) {
-                    tokenLength = possibleTokenLength;
-                    tokenString = SearchBufferTest.substring(i, i + tokenLength);
-                    tokenChar = LookAheadTest.charAt(tokenLength);
-                    tokenOffset = lenghtOfBuffer - i;
+    
+                tokenCount++;
+                System.out.println("Token = ( " + tokenOffset + ", " + tokenLength + ", " + tokenByte + " )");
+                
+
+
+                //sliding window here
+                if(tokenLength == 0){
+                    for(int i = 0; i < searchBuffer.length - 1; i++){
+                        searchBuffer[i] = searchBuffer[i+1]; 
+                    }
+                    searchBuffer[searchBuffer.length-1] = lookAheadBuffer[0];
+    
+                    for(int i = 0; i < lookAheadBuffer.length - 1; i++){
+                        lookAheadBuffer[i] = lookAheadBuffer[i+1]; 
+                    }
+                    lookAheadBuffer[lookAheadBuffer.length - 1] = (byte)fin.read();
+                }else{
+                    for(int k = 0; k < tokenLength+1; k++){
+                        for(int i = 0; i < searchBuffer.length - 1; i++){
+                            searchBuffer[i] = searchBuffer[i+1]; 
+                        }
+                        searchBuffer[searchBuffer.length-1] = lookAheadBuffer[0];
+        
+                        for(int i = 0; i < lookAheadBuffer.length - 1; i++){
+                            lookAheadBuffer[i] = lookAheadBuffer[i+1]; 
+                        }
+                        lookAheadBuffer[lookAheadBuffer.length - 1] = (byte)fin.read();
+                    }
                 }
             }
-        }
 
-        System.out.println("Token = ( " + tokenOffset + ", " + tokenLength + ", " + tokenChar + " )");
-    }
+            System.out.println("tokenCount = " + tokenCount);
+            
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the file name: ");
-        String fileName = scanner.nextLine(); // Read the file name from the user
+            fin.close();
 
-        try (FileInputStream fin = new FileInputStream(fileName)) {
-            System.out.println("File opened successfully!");
-            // You can add logic here to process the file contents if needed
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("An error occurred while reading the file: " + e.getMessage());
         }
 
-        // Example calls to the token-making function
-        tokenMakingTest("sir sid eatman easily t", "eases sea sick seals   ");
-        System.out.println("======================================");
+        
+    }
 
-        tokenMakingTest(" sid eatman easily teas", "es sea sick seals   ");
-        System.out.println("======================================");
+    public static void main(String[] args) {
+        
+        tokenMakingTest("File1.html");
 
-        tokenMakingTest("sid eatman easily tease", "s sea sick seals   ");
-        System.out.println("======================================");
-
-        tokenMakingTest("id eatman easily teases", " sea sick seals   ");
-        System.out.println("======================================");
-
-        tokenMakingTest("d eatman easily teases ", "sea sick seals   ");
-
-        scanner.close();
     }
 }
